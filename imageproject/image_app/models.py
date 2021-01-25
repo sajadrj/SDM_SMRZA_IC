2495c380d3ae274e876793f15a71286da420e1e8
me76ss <34098696+me76ss@users.noreply.github.com>
2021-01-25 13:36:41 +0330

    Add files via upload

---
+++ imageproject/image_app/models.py
@@ -0,0 +1,6 @@
+# Create your models here.
+
+from django.db import models
+
+class MyImage(models.Model):
+	model_pic = models.ImageField(upload_to = '', default = 'none/no-img.jpg')
---
+++ imageproject/image_app/views.py
@@ -0,0 +1,12 @@
+#from django.shortcuts import render
+
+# Create your views here.
+
+from .serializers import imageSerializer
+from rest_framework.generics import (CreateAPIView)
+from image_app.models import MyImage
+
+
+class ImageCreateAPIView(CreateAPIView):
+	serializer_class = imageSerializer
+	queryset = MyImage.objects.all()
